d86708f1d3e98c5ee44ff78f45526c90015a67d7
Renma Hirano <everydaysai@gmail.com>
2021-06-24 08:41:03 +0900

    画像を投稿できるアプリケーションの開発

--- SNSapplication/ViewController.swift
+++ SNSapplication/ViewController.swift
@@ -9,6 +9,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    @IBAction func showActivityView(_ sender: UIBarButtonItem) {
+        let controller = UIActivityViewController(activityItems: [imageView.image!], applicationActivities: nil)
+        self.present(controller, animated: true, completion: {() -> Void in print("表示完了")})
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
